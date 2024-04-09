c2c845582302e0d77b033bb7a85ae14aa4154c9c
Mojmír Hegedüš <mojmirr@MacBook-Air-uzivatela-Mojmir.local>
2024-04-09 20:57:16 +0200

    initial commit

---
+++ array.js
@@ -0,0 +1,15 @@
+let fruits = ["orange", "apple", "apple", "orange", "orange"];
+let apples = [];
+let oranges = [];
+
+function sortfruit() {
+  for (let i = 0; i < fruits.length; i++) {
+    if (fruit[i] === "orange") {
+      apples.push(i);
+    } else {
+      oranges.push(i);
+    }
+  }
+}
+
+console.log(oranges);
